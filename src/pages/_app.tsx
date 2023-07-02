c9148ef9b1e0ce0070e7de33796632f9e30b41a9
uuaaxyxy <uuaaxyxy968@gmail.com>
2023-07-03 02:42:28 +0900

    ✨ ヘッダーコンポーネント実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import Header from './components/Header';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
-const Home: React.FC = () => {
+const Home = () => {
+  const { data: session } = useSession();
+  // セッション情報が確認できる.
+  console.log(session);
+
+  // セッションによって表示切り替え.
+  // ログインしていればここ.
+  if (session) {
+    return (
+      <>
+        Singed in as {session.user?.name} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  // ログインしてなかったらこれ.
   return (
     <>
-      <h1>Hello!!</h1>
+      Noto signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
     </>
   );
 };
